3ed3d1c91deeb4dce44557d1db2145ed337838cf
britz2142 <britz2142@gmail.com>
2015-05-22 02:14:25 +0900

    시작 메뉴 Scene 추가, 전환기능 추가

---
+++ Assets/Dummy/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxSpeed = 10;
+    public bool facingRight = true;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+       Rigidbody2D player = GetComponent<Rigidbody2D>();
+
+	    if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+           player.AddForce(new Vector2(0,jumpForce));
+        }
+	}
+
+    void FixedUpdate()
+    {
+       grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+       Rigidbody2D player = GetComponent<Rigidbody2D>();
+        
+        float move = Input.GetAxis("Horizontal");
+        player.velocity = new Vector2(move * maxSpeed, player.velocity.y);
+
+        if (move < 0 && facingRight)
+            Filp();
+        else if (move > 0 && !facingRight)
+            Filp();
+    }
+
+    void Filp()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        GameObject flash = GameObject.Find("Flash");
+        Vector3 flashScale = flash.transform.localScale;
+        Quaternion theRotate;
+        float z = flash.transform.localRotation.z;
+
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        flashScale.x *= -1;
+       flash.transform.localScale = flashScale;
+
+        if (!facingRight)
+        {
+            theRotate = Quaternion.Euler(0, 0, z+180);
+        }
+        else
+        {
+            theRotate = Quaternion.Euler(0, 0, z);
+        }
+      
+      flash.transform.localRotation = theRotate;
+
+    }
+}
---
+++ Assets/Dummy/RotateAngle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAngle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 flashPoint = transform.position;
+        Vector3 mousePoint = Input.mousePosition; // 각 지점 저장
+
+        mousePoint.z = flashPoint.z - Camera.main.transform.position.z; //z축의 좌표를 맞춘다.
+        Vector3 newPoint = Camera.main.ScreenToWorldPoint(mousePoint); //스크린의 좌표를 게임 내 월드 좌표로 전환한다.
+
+        float x = newPoint.x - flashPoint.x;
+        float y = newPoint.y - flashPoint.y;
+
+        float seta= Mathf.Atan2(y, x) * Mathf.Rad2Deg; // 아크탄젠트로 seta를 구하고, rad2deg로 라디안 값을 디그리 값으로 바꾼다.
+
+     if (!GameObject.Find("Player").GetComponent<PlayerController>().facingRight)
+        {
+            seta *= -1;
+        }
+
+     Debug.Log(seta);
+        //if (seta > -90 && seta < 90)
+            transform.rotation = Quaternion.Euler(0f,0f,seta);
+
+    }
+}
