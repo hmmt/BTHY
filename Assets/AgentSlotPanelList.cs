20c92afb2513e489978818cd9b54035c2c1b423f
britz2142 <britz2142@gmail.com>
2015-07-25 22:20:19 +0900

    1. 직원 리스트 UI 추가 2. 환상체 이미지 변경

---
+++ Assets/AgentSlotPanelList.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentSlotPanelList : MonoBehaviour {
+
+    public UnityEngine.UI.Image agentIcon;
+    public UnityEngine.UI.Button agentInfoButton;
+    public UnityEngine.UI.Text agentName;
+    public UnityEngine.UI.Text agentHP;
+    public UnityEngine.UI.Text agentMental;
+    public UnityEngine.UI.Text agentSefria;
+    public UnityEngine.UI.Text agentLevel;
+
+}
