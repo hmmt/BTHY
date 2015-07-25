e2656f7846468554ded4c93fcc48b75aad5d19f0
britz2142 <britz2142@gmail.com>
2015-07-25 22:22:12 +0900

    1. 직원 리스트 UI 추가 2. 환상체 이미지 추가

--- Assets/Scripts/UI/Slot/AgentSlotPanel.cs
+++ Assets/Scripts/UI/Slot/AgentSlotPanel.cs
@@ -6,6 +6,7 @@ public class AgentSlotPanel : MonoBehaviour {
     public UnityEngine.UI.Text agentName;
     public UnityEngine.UI.Text agentHealth;
     public UnityEngine.UI.Text agentMental;
+    public UnityEngine.UI.Text agentLevel;
 	public UnityEngine.UI.Button skillButton1;
 	public UnityEngine.UI.Button skillButton2;
 	public UnityEngine.UI.Button skillButton3;
