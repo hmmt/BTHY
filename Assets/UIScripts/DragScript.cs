ccdf7e5b5e83da64f8cab63518f2a3bb845ca327
HyungjunJu <nighthawk189@gmail.com>
2015-09-15 23:58:55 +0900

    Start Stage UI 변경

---
+++ Assets/UIScripts/DragScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public static GameObject itemDragged;
+    Vector3 startPos;
+    public GameObject moveImage;
+    Transform startParent;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startParent = transform.parent;
+        //itemDragged = gameObject;
+        itemDragged = (GameObject)Instantiate(moveImage);
+        itemDragged.transform.SetParent(transform.parent);
+        startPos = transform.position;
+        itemDragged.transform.position = startPos;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        itemDragged.transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //itemDragged = null;
+        //itemDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (itemDragged.transform.parent == startParent) {
+            Destroy(itemDragged);
+        }
+       
+        //itemDragged = null;
+        //transform.position = startPos;
+    }
+}
